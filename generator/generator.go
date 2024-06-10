3ddfa051f5e5a0289dcd9866929cf8537e2ec5d1
Kishan Jeetun <jeetun@gmail.com>
2024-06-11 01:40:42 +1000

    fix minor typos

--- generator/generator.go
+++ generator/generator.go
@@ -2,20 +2,20 @@ package generator
 
 func GenerateEvenNumbers(limit int) <-chan int {
 	// have a channel to which we can push even number generated
-	eventNumChan := make(chan int)
+	evenNumChan := make(chan int)
 
 	go func() {
-		// last event number to generate should be provided limit * 2
+		// last even number to generate should be the provided limit * 2
 		lastEvenNum := limit * 2
 
-		// generate event numbers to the limit
+		// generate even numbers up to the limit
 		for i := 2; i <= lastEvenNum; i += 2 {
-			eventNumChan <- i
+			evenNumChan <- i
 		}
 
 		// close channel when done
-		close(eventNumChan)
+		close(evenNumChan)
 	}()
 
-	return eventNumChan
+	return evenNumChan
 }
